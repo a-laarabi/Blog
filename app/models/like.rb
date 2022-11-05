cdcfc83645ff37895b62eda4b82bc158e5930f97
Anasse LAARABI <contact.laarabi@gmail.com>
2022-11-05 12:14:10 +0100

    Fix merge conflicts

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :user, foreign_key: 'author_id', class_name: 'User'
+  belongs_to :post, foreign_key: 'post_id'
   after_save :update_likes_counter
 
   private
